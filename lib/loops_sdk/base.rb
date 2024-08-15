b78886a3e3509bc86b6233b41e7f65f9e8eb2411
Dan <dan@loops.so>
2024-08-15 14:50:56 +0300

    Rename LoopsAPIError

--- lib/loops_sdk/base.rb
+++ lib/loops_sdk/base.rb
@@ -10,14 +10,14 @@ module LoopsSdk
         when 200
           JSON.parse(response.body)
         when 400, 404, 405, 409, 500
-          raise LoopsAPIError.new(response.status, response.body)
+          raise APIError.new(response.status, response.body)
         else
-          raise LoopsAPIError.new(response.status, "Unexpected error occurred")
+          raise APIError.new(response.status, "Unexpected error occurred")
         end
       end
 
       def make_request(method, path, params = {}, body = nil)
-        response = Loops.configuration.connection.send(method, path, params) do |req|
+        response = LoopsSdk.configuration.connection.send(method, path, params) do |req|
           req.body = body.to_json if body
         end
         handle_response(response)
@@ -25,7 +25,9 @@ module LoopsSdk
     end
   end
 
-  class LoopsAPIError < StandardError
+  # The `APIError` class in Ruby represents an error that occurs during an API request with specific
+  # status and response body information.
+  class APIError < StandardError
     attr_reader :status, :body
 
     def initialize(status, body)
