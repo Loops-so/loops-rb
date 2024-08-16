531a28f873e177f8ab4cfdb1c87d68af30cb5196
Dan <dan@loops.so>
2024-08-16 10:51:29 +0300

    Convert `content_type`

--- lib/loops_sdk/transactional.rb
+++ lib/loops_sdk/transactional.rb
@@ -4,6 +4,9 @@ module LoopsSdk
   class Transactional < Base
     class << self
       def send(transactional_id:, email:, add_to_audience: false, data_variables: {}, attachments: [])
+        attachments = attachments.map do |attachment|
+          attachment.transform_keys { |key| key == :content_type ? :contentType : key }
+        end
         email_data = {
           transactionalId: transactional_id,
           email: email,
